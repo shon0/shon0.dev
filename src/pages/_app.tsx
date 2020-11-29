9dc4f8729a08498fb921f4a823c96e49dbe66ee9
ShonoKoga <shono.koga@gmail.com>
2020-11-30 00:24:29 +0900

    Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import 'styles/global.css';
+import 'styles/prism.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
