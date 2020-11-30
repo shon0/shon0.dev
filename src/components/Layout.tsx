2062ccff707371845c666f9f358c141b419142bf
ShonoKoga <shono.koga@gmail.com>
2020-11-30 23:59:56 +0900

    レスポンシブ対応

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,12 +8,13 @@ type Props = {
 
 const Component: React.FC<Props> = ({ title, children }) => {
   return (
-    <div className="max-w-screen-md mx-auto px-7">
+    <div className="max-w-screen-md mx-auto px-4 sm:px-7">
       <Head>
         <title>{title ? `${title} | ${SITE_TITLE}` : SITE_TITLE}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
-      <header className="flex-1 pt-12 mb-16">
+      <header className="flex-1 pt-12 mb-10">
         <div className="flex items-center justify-start">
           <h1 className="text-2xl font-bold leading-normal font-montserrat">
             <Link href="/">
