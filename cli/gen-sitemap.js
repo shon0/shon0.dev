0a544954127a91564e2525fc7d6f958686b584b8
ShonoKoga <shono.koga@gmail.com>
2020-12-10 18:15:56 +0900

    Fix sitemap generator

--- cli/gen-sitemap.js
+++ cli/gen-sitemap.js
@@ -1,9 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const sitemap = require('nextjs-sitemap-generator');
+const fs = require('fs');
+
+const builtDir = fs.existsSync(__dirname + '/../.next/serverless')
+  ? __dirname + '/../.next/serverless/pages'
+  : __dirname + '/../.next/server/pages';
 
 sitemap({
   baseUrl: 'https://shon0.dev',
-  pagesDirectory: __dirname + '/../.next/server/pages',
+  pagesDirectory: builtDir,
   targetDirectory: 'public/',
   ignoredExtensions: ['js', 'map'],
   ignoredPaths: [
