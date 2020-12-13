e9cda44a0ee4db2fb1f64df24832084cc0a2c10e
ShonoKoga <shono.koga@gmail.com>
2020-12-13 13:40:56 +0900

    Tweak

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import 'styles/global.css'
+import 'styles/prism.css'
+
 import { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from 'lib/gtag'
-import 'styles/global.css'
-import 'styles/prism.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter()
