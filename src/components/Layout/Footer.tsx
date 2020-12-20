a86fd52abcb212fb9de5536f7108950f4c443080
shon0 <shono.koga@gmail.com>
2020-12-20 23:58:26 +0900

    Fix icon size

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -19,7 +19,7 @@ const Component: React.FC = () => {
           rel="noopener noreferrer"
           className="text-black hover:text-gray-700"
         >
-          <Icon.Github width="16" height="16" />
+          <Icon.Github width="20" height="20" />
         </a>
         <a
           href={TWITTER_URL}
@@ -27,7 +27,7 @@ const Component: React.FC = () => {
           rel="noopener noreferrer"
           className="text-black hover:text-gray-700 ml-3"
         >
-          <Icon.Twitter width="16" height="16" />
+          <Icon.Twitter width="20" height="20" />
         </a>
       </div>
     </footer>
