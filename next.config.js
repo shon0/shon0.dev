a400a929afc7124772def8abd8dc2d2f846290e3
shon0 <shono.koga@gmail.com>
2021-03-20 23:18:53 +0900

    ImageOptimization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  trailingSlash: true,
+  images: {
+    loader: 'imgix',
+    path: 'https://images.microcms-assets.io',
+  },
   experimental: {
     optimizeFonts: true,
   },
