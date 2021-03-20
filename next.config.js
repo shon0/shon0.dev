a7d08da75934841bdbc7fd0db8a3f6be2f952d72
shon0 <shono.koga@gmail.com>
2021-03-21 00:16:28 +0900

    Fix image config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   trailingSlash: true,
   images: {
-    loader: 'imgix',
-    path: 'https://images.microcms-assets.io',
+    domains: ['https://images.microcms-assets.io'],
   },
   experimental: {
     optimizeFonts: true,
   },
-};
+}
